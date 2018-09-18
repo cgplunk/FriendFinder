3eb3c4ae2ce505da82662c47531ecdea2cf934bf
codygplunkett <cody.plunkett@yahoo.com>
2018-09-17 20:19:22 -0500

    finished

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,4 +5,39 @@ module.exports = function (app) {
     app.get("/app/friends", function(req, res) {
         res.json(friends);
     });
-}
+
+    app.post("/api/friends", function(req, res) {
+
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+          };
+      
+          var userData = req.body;
+          var userScores = userData.scores;
+          
+          console.log(userScores);
+
+          var totalDifference = 0;
+      
+          for (var i = 0; i < friends.length; i++) {
+            console.log(friends[i]);
+            totalDifference = 0;
+      
+            // We then loop through all the scores of each friend
+            for ( var j = 0; j < friends[i].scores[j]; j++) {
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+                if (totalDifference <= bestMatch.friendDifference) {
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.friendDifference = totalDifference;
+                }
+            }
+        }    
+
+        friends.push(userData);
+
+        res.json(bestMatch);
+    });
+}   
--- server.js
+++ server.js
@@ -4,19 +4,12 @@ var bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 8080;
 
-// create application/json parser
-var jsonParser = bodyParser.json()
- 
-// create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// parse various different custom JSON types as JSON
 app.use(bodyParser.json({ type: 'application/*+json' }))
- 
-// parse some custom thing into a Buffer
+
 app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
  
-// parse an HTML body into a string
 app.use(bodyParser.text({ type: 'text/html' }))
 
 require("./app/routing/apiRoutes.js")(app);
